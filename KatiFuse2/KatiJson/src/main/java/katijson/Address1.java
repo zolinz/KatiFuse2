f8610c62d33582b7461697ec7c750890b67f8d84
ZOLTANKOVACS <zoltankovacs@nbnco.com.au>
2016-11-19 15:44:03 +1100

    added Json module

---
+++ KatiFuse2/KatiJson/src/main/java/katijson/Address1.java
@@ -0,0 +1,47 @@
+package katijson;
+
+/**
+ * Created by Zoli on 19/11/2016.
+ */
+public class Address1 {
+
+    private String housenumber;
+
+    public String getHousenumber() {
+        return this.housenumber;
+    }
+
+    public void setHousenumber(String housenumber) {
+        this.housenumber = housenumber;
+    }
+
+    private String streetname;
+
+    public String getStreetname() {
+        return this.streetname;
+    }
+
+    public void setStreetname(String streetname) {
+        this.streetname = streetname;
+    }
+
+    private String postcode;
+
+    public String getPostcode() {
+        return this.postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    private String country;
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
---
+++ KatiFuse2/KatiJson/src/main/java/katijson/ProcessJson.java
@@ -0,0 +1,36 @@
+package katijson;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Zoli on 19/11/2016.
+ */
+public class ProcessJson {
+
+    public static void main(String ... args){
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+
+            // Convert JSON string from file to Object
+            Address1 addr1 = mapper.readValue(new File("KatiJson/src/main/resources/data/Address1.json"), Address1.class);
+            System.out.println(addr1);
+
+            //Pretty print
+            String prettyAddr1 = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(addr1);
+            System.out.println(prettyAddr1);
+
+        } catch (JsonGenerationException e) {
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
