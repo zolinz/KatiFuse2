04cb4c3be0c1bcae6752299d92d6aac3759e1d59
ZOLTANKOVACS <zoltankovacs@nbnco.com.au>
2016-11-19 15:11:09 +1100

    added output file processing

--- KatiFuse2/KatiXmlProcessing/src/main/java/xmltojava/ProcessXml.java
+++ KatiFuse2/KatiXmlProcessing/src/main/java/xmltojava/ProcessXml.java
@@ -4,6 +4,7 @@ import kati.addr1.Address1;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 
@@ -15,16 +16,27 @@ public class ProcessXml {
     public static void main(String ... args){
         try {
 
-            File file = new File("KatiXmlProcessing/" +
-                    "src/main/resources/data/Address1.xml");
+            File inputFile = new File("KatiXmlProcessing/src/main/resources/data/Address1.xml");
 
-
+            //generic step
             JAXBContext jaxbContext = JAXBContext.newInstance(Address1.class);
-
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            Address1 addr1 = (Address1) jaxbUnmarshaller.unmarshal(file);
+
+            // reading from file
+            Address1 addr1 = (Address1) jaxbUnmarshaller.unmarshal(inputFile);
             System.out.println(addr1.getCountry());
             System.out.println(addr1.getStreetname());
+
+            addr1.setCountry("Nz");
+
+
+            File fileOut = new File("KatiXmlProcessing/src/main/resources/data/Address1_new.xml");
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            jaxbMarshaller.marshal(addr1, fileOut);
+
+
+
 
         } catch (JAXBException e) {
             e.printStackTrace();
