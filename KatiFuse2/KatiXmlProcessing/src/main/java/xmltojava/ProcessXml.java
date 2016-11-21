59217e1047d0f1a98f5b9d3da16263fad7abae3f
ZOLTANKOVACS <zoltankovacs@nbnco.com.au>
2016-11-21 20:26:01 +1100

    added xml transformation

--- KatiFuse2/KatiXmlProcessing/src/main/java/xmltojava/ProcessXml.java
+++ KatiFuse2/KatiXmlProcessing/src/main/java/xmltojava/ProcessXml.java
@@ -1,6 +1,7 @@
 package xmltojava;
 
 import kati.addr1.Address1;
+import kati.addr2.Address2;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -16,24 +17,28 @@ public class ProcessXml {
     public static void main(String ... args){
         try {
 
-            File inputFile = new File("KatiXmlProcessing/src/main/resources/data/Address1.xml");
+            File inputFile = new File("KatiXmlProcessing/src/main/resources/data/Address1_in.xml");
 
             //generic step
-            JAXBContext jaxbContext = JAXBContext.newInstance(Address1.class);
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            JAXBContext jaxbContextIn = JAXBContext.newInstance(Address1.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContextIn.createUnmarshaller();
 
             // reading from file
             Address1 addr1 = (Address1) jaxbUnmarshaller.unmarshal(inputFile);
-            System.out.println(addr1.getCountry());
-            System.out.println(addr1.getStreetname());
 
-            addr1.setCountry("Nz");
+            //transformation
+
+            Address2 addr2 = new Address2();
+
+            addr2.setNameOfStreet(addr1.getStreetname());
+            addr2.setNumberOfStreet(addr1.getHousenumber());
 
 
-            File fileOut = new File("KatiXmlProcessing/src/main/resources/data/Address1_new.xml");
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+            File fileOut = new File("KatiXmlProcessing/src/main/resources/data/Address2_out.xml");
+            JAXBContext jaxbContextOut = JAXBContext.newInstance(Address2.class);
+            Marshaller jaxbMarshaller = jaxbContextOut.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            jaxbMarshaller.marshal(addr1, fileOut);
+            jaxbMarshaller.marshal(addr2, fileOut);
 
 
 
